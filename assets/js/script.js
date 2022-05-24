b7c57533730a5f9b35a1766d72821fe10535da18
Bin O <bin.ostrowski@gmail.com>
2022-05-23 18:33:40 -0700

    add random lowerCaseCar

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,32 @@
 // Assignment code here
+function generatePassword(){ 
+    // ask user how long should pawword be (and validate length)
+    // should it contain special characters?
+    // should it contain numbers
+    // should it contain capitals
+    // generate a random password from selected characters.
 
+    var lowerCaseChar = 'abcdefghijklmnopqrstuvwxyz'
+    var randomNum = getRandomInt(lowerCaseChar.length + 1);
+    console.log('random char is: ', lowerCaseChar[randomNum]);
+    function getRandomInt(max) {
+        return Math.floor(Math.random() * max);
+    }
+
+}
+// var passwordPrompt = function(generatePassword) {
+//     window.prompt(
+//         "Select which criteria to include in the password. Please enter 1 for PASSWORD LENGTH or 2 for CHARACTER TYPES."
+//     );
+//     passwordPrompt = parseInt (passwordPrompt);
+//     switch(passwordPrompt) {
+//         case 1:
+//             window.prompt(
+//                 "Please choose a length of at least 8 characters and no more than 128 characters."
+//             )
+//     }
+
+// };
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -15,3 +42,4 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
