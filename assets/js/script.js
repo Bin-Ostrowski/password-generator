05bbdcb50638c574d35ee157ee7e7a01bd17ef69
Bin O <bin.ostrowski@gmail.com>
2022-05-25 00:55:01 -0700

    add random functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,37 @@
 //Assignment code here
 function generatePassword(){ 
-    
-  // ask user how long should pawword be (and validate length)
+  
+  //generating random characters
+  var lowerCaseChar = 'abcdefghijklmnopqrstuvwxyz';
+    var randomInt = getRandomInt(lowerCaseChar.length + 1);
+    console.log('random char is: ', lowerCaseChar[randomInt]);
+    function getRandomInt(max) {
+        return Math.floor(Math.random() * max);
+    }
+  
+  var number = '1234567890';
+  var randomNum = getRandomNum(number.length + 1);
+  console.log('random num is: ', number[randomNum]);
+  function getRandomNum(max) {
+      return Math.floor(Math.random() * max);
+  }
 
-var chooseLength = prompt("How many characters should your password be? Choose a number of at least 8 and no more than 128.");
+  var specialChar= "!#$%&'()*+,-./:;<=>?@_`{|}~";
+  var randomSpecialChar = getRandomSpecialChar(specialChar.length + 1);
+  console.log('random special char is: ', specialChar[randomSpecialChar]);
+  function getRandomSpecialChar(max) {
+      return Math.floor(Math.random() * max);
+  }
+
+  var capitalChar = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  var randomCapitalChar = getRandomCapitalChar(capitalChar.length + 1);
+  console.log('random capital char is: ', capitalChar[randomCapitalChar]);
+  function getRandomCapitalChar(max) {
+      return Math.floor(Math.random() * max);
+  }
+
+  // ask user how long should pawword be (and validate length)
+  var chooseLength = prompt("How many characters should your password be? Choose a number of at least 8 and no more than 128.");
         if (!chooseLength || chooseLength === null) {
       alert("Please fill in a number between 1 and ___");
       return false;
@@ -16,84 +44,42 @@ var chooseLength = prompt("How many characters should your password be? Choose a
   
   console.log("password should be " + passwordLength + " characters long.");
 
-  // should it contain numbers
-  var containNums = confirm("Should your password contain numbers? Click 'OK' for yes or 'CANCEL' for no."); 
-    if (!containNums) {
-      console.log("No, don't include numbers.")
+
+  // should it contain numbers this should be a function
+  var containNum = confirm("Should your password contain numbers? Click 'OK' for yes or 'CANCEL' for no."); 
+    if (!containNum) {
+      console.log("No, don't include numbers.");
     } else {
-      var yesNums = '1234567890'
-      console.log("should include letters and numbers")
-    } 
+      
+      console.log("should include letters and numbers");
+    };
   
+
   // should it contain special characters?
-  var containSpecialChars = window.confirm ("Should your password contain special characters? Click 'OK' for yes or 'CANCEL' for no."); 
-    if (!containSpecialChars) {
-      //pass
-      console.log("No, don't include special characters.")
+  var containSpecialChar = window.confirm ("Should your password contain special characters? Click 'OK' for yes or 'CANCEL' for no."); 
+    if (!containSpecialChar) {
+      console.log("No, don't include special characters.");
     } else {
-      containSpecialChars = "should include special characters"
-      var yesSpecialChars= "!#$%&'()*+,-./:;<=>?@_`{|}~"
-      console.log("Yes, include special characters.")
-    } 
+      console.log("Yes, include special characters.");
+    };
 
   // should it contain capitals
-  var containCaps = window.confirm ("Should your password capitals? Click 'OK' for yes or 'CANCEL' for no."); 
-  if (!containCaps) {
-    //pass
-    console.log("No, don't include capitals.")
+  var containCap = window.confirm ("Should your password capitals? Click 'OK' for yes or 'CANCEL' for no."); 
+  if (!containCap) {
+    console.log("No, don't include capitals.");
   } else {
-    var yesCaps = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-    console.log("Yes, include capitals.")
+    console.log("Yes, include capitals.");
   } 
 
   //generate a random password from selected characters.
-
-
-  var lowerCaseChar = 'abcdefghijklmnopqrstuvwxyz'
-    var randomNum = getRandomInt(lowerCaseChar.length + 1);
-    console.log('random char is: ', lowerCaseChar[randomNum]);
-    function getRandomInt(max) {
-        return Math.floor(Math.random() * max);
-    }
-
-
-  
-
+  if ((containNum && containSpecialChar) && containCap) {
+    window.alert("Your password is " + lowerCaseChar[randomInt] + 
+      number[randomNum] + 
+      specialChar[randomSpecialChar] + 
+      capitalChar[randomCapitalChar]);
+  }
 }
-
-  
-//   if ((containNums && containSpecialChars) && containCaps) {
-//    // function allThree() {
-//       console.log("numbers, special, caps");
-//    // } 
-//   } else if (!containNums) {
-//    // function noNums() {
-//       console.log("no numbers, special, caps");
-//     //}
-//   } else if (!containSpecialChars) {
-//     //function noSpecialChars() {
-//       console.log("nums, no special, caps");
-//     //}
-//   } else if (!containCaps) {
-//     //function noCaps() {
-//       console.log("numbers, special, no caps");
-//     //}
-//   } else {
-//     console.log("no nums, no special, no caps")
-//   };
-// };
     
-    
-   
-
-    var lowerCaseChar = 'abcdefghijklmnopqrstuvwxyz'
-    var randomNum = getRandomInt(lowerCaseChar.length + 1);
-    console.log('random char is: ', lowerCaseChar[randomNum]);
-    function getRandomInt(max) {
-        return Math.floor(Math.random() * max);
-    }
-
-
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
