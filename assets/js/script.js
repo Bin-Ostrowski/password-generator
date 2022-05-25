454c719a8652255d0338b6c89acad9cb0f567841
Bin O <bin.ostrowski@gmail.com>
2022-05-24 23:17:18 -0700

    add promts for password criteria

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,90 @@
-// Assignment code here
+//Assignment code here
 function generatePassword(){ 
-    // ask user how long should pawword be (and validate length)
-    // should it contain special characters?
-    // should it contain numbers
-    // should it contain capitals
-    // generate a random password from selected characters.
+    
+  // ask user how long should pawword be (and validate length)
+
+var chooseLength = prompt("How many characters should your password be? Choose a number of at least 8 and no more than 128.");
+        if (!chooseLength || chooseLength === null) {
+      alert("Please fill in a number between 1 and ___");
+      return false;
+    } else if (chooseLength < 8 || chooseLength > 128) {
+      alert("Please choose a number of at least 8 and no more than 128.");
+      return false;
+    } else {
+    var passwordLength = parseInt(chooseLength);
+    };
+  
+  console.log("password should be " + passwordLength + " characters long.");
+
+  // should it contain numbers
+  var containNums = confirm("Should your password contain numbers? Click 'OK' for yes or 'CANCEL' for no."); 
+    if (!containNums) {
+      console.log("No, don't include numbers.")
+    } else {
+      var yesNums = '1234567890'
+      console.log("should include letters and numbers")
+    } 
+  
+  // should it contain special characters?
+  var containSpecialChars = window.confirm ("Should your password contain special characters? Click 'OK' for yes or 'CANCEL' for no."); 
+    if (!containSpecialChars) {
+      //pass
+      console.log("No, don't include special characters.")
+    } else {
+      containSpecialChars = "should include special characters"
+      var yesSpecialChars= "!#$%&'()*+,-./:;<=>?@_`{|}~"
+      console.log("Yes, include special characters.")
+    } 
+
+  // should it contain capitals
+  var containCaps = window.confirm ("Should your password capitals? Click 'OK' for yes or 'CANCEL' for no."); 
+  if (!containCaps) {
+    //pass
+    console.log("No, don't include capitals.")
+  } else {
+    var yesCaps = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    console.log("Yes, include capitals.")
+  } 
+
+  //generate a random password from selected characters.
+
+
+  var lowerCaseChar = 'abcdefghijklmnopqrstuvwxyz'
+    var randomNum = getRandomInt(lowerCaseChar.length + 1);
+    console.log('random char is: ', lowerCaseChar[randomNum]);
+    function getRandomInt(max) {
+        return Math.floor(Math.random() * max);
+    }
+
+
+  
+
+}
+
+  
+//   if ((containNums && containSpecialChars) && containCaps) {
+//    // function allThree() {
+//       console.log("numbers, special, caps");
+//    // } 
+//   } else if (!containNums) {
+//    // function noNums() {
+//       console.log("no numbers, special, caps");
+//     //}
+//   } else if (!containSpecialChars) {
+//     //function noSpecialChars() {
+//       console.log("nums, no special, caps");
+//     //}
+//   } else if (!containCaps) {
+//     //function noCaps() {
+//       console.log("numbers, special, no caps");
+//     //}
+//   } else {
+//     console.log("no nums, no special, no caps")
+//   };
+// };
+    
+    
+   
 
     var lowerCaseChar = 'abcdefghijklmnopqrstuvwxyz'
     var randomNum = getRandomInt(lowerCaseChar.length + 1);
@@ -13,20 +93,6 @@ function generatePassword(){
         return Math.floor(Math.random() * max);
     }
 
-}
-// var passwordPrompt = function(generatePassword) {
-//     window.prompt(
-//         "Select which criteria to include in the password. Please enter 1 for PASSWORD LENGTH or 2 for CHARACTER TYPES."
-//     );
-//     passwordPrompt = parseInt (passwordPrompt);
-//     switch(passwordPrompt) {
-//         case 1:
-//             window.prompt(
-//                 "Please choose a length of at least 8 characters and no more than 128 characters."
-//             )
-//     }
-
-// };
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -42,4 +108,3 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
